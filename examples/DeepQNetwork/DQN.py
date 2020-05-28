8f06e97a03a262e31bd0f595f86fbf35b7d1e7fc
Vladimir <10669582+Wovchena@users.noreply.github.com>
2020-05-29 02:27:06 +0300

    add TfAdapter

--- examples/DeepQNetwork/DQN.py
+++ examples/DeepQNetwork/DQN.py
@@ -119,10 +119,34 @@ def update_target_param():
     return tf.group(*ops, name='update_target_network')
 
 
+class TfAdapter:
+    def __init__(self, image_size, frame_history):
+        self.model = Model(image_size, frame_history, get_player().action_space.n)
+        self.placeholders = tuple(tf.placeholder(tensor_spec.dtype, shape=tensor_spec.shape, name=tensor_spec.name)
+                             for tensor_spec in self.model.inputs())
+        self.opt = tf.train.RMSPropOptimizer(1e-3, decay=0.95, momentum=0.95, epsilon=1e-2)
+        self.train_op = self.opt.minimize(self.model.build_graph(*self.placeholders))
+
+        self.update_target_op = update_target_param()
+
+        with tf.name_scope('tower-pred-0/'):
+            self.model.build_graph(*self.placeholders)
+        self.sess = tf.Session()
+        self.sess.run(tf.initialize_all_variables())
+
+        self.infer = self.sess.make_callable(fetches=['tower-pred-0/Qvalue:0'], feed_list=['tower-pred-0/state:0'])
+
+    def train_step(self, batch):
+        self.sess.run(self.train_op, feed_dict=dict(zip(self.placeholders, batch)))
+
+    def update_target(self):
+        self.update_target_op.run()
+
+
 def main():
     BATCH_SIZE = 64
     IMAGE_SIZE = (84, 75)
-    FRAME_HISTORY = 4
+    FRAME_HISTORY = 4   
     UPDATE_FREQ = 4  # the number of new state transitions per parameter update (per training step)
     MEMORY_SIZE = 10**6
     INIT_MEMORY_SIZE = MEMORY_SIZE // 20
@@ -131,42 +155,33 @@ def main():
     START_EPSILON = 1.0
     STOP_EPSILON_DECAY_AT = 250000
 
-    model = Model(IMAGE_SIZE, FRAME_HISTORY, get_player().action_space.n)
-    placeholders = tuple(tf.placeholder(tensor_spec.dtype, shape=tensor_spec.shape, name=tensor_spec.name)
-                         for tensor_spec in model.inputs())
-    opt = tf.train.RMSPropOptimizer(1e-3, decay=0.95, momentum=0.95, epsilon=1e-2)
-    train_op = opt.minimize(model.build_graph(*placeholders))
-    update_target_param_op = update_target_param()
-    with tf.name_scope('tower-pred-0/'):
-        model.build_graph(*placeholders)
+    adapter = TfAdapter(IMAGE_SIZE, FRAME_HISTORY)
     summary_writer = tf.summary.FileWriter(datetime.datetime.now().strftime('logs/%d-%m-%Y_%H-%M'))
-    with tf.Session() as sess:
-        sess.run(tf.initialize_all_variables())
-        expreplay = ExpReplay(
-            sess.make_callable(fetches=['tower-pred-0/Qvalue:0'], feed_list=['tower-pred-0/state:0']),
-            get_player=get_player,
-            num_parallel_players=NUM_PARALLEL_PLAYERS,
-            state_shape=model.state_shape,
-            batch_size=BATCH_SIZE,
-            memory_size=MEMORY_SIZE,
-            init_memory_size=INIT_MEMORY_SIZE,
-            update_frequency=UPDATE_FREQ,
-            history_len=FRAME_HISTORY,
-            state_dtype=model.state_dtype.as_numpy_dtype
-        )
-        expreplay._before_train()
-        for step_idx, batch in enumerate(expreplay):
-            sess.run(train_op, feed_dict=dict(zip(placeholders, batch)))
-            if expreplay.exploration > MIN_EPSILON:
-                expreplay.exploration -= (START_EPSILON - MIN_EPSILON) / STOP_EPSILON_DECAY_AT
-            if step_idx > 0 and step_idx % 5000 == 0:
-                update_target_param_op.run()
-                mean, max = expreplay.runner.reset_stats()
-                summary_writer.add_summary(
-                    tf.Summary(value=(
-                        tf.Summary.Value(tag='expreplay/mean_score', simple_value=mean),
-                        tf.Summary.Value(tag='expreplay/max_score', simple_value=max))),
-                    step_idx)
+    expreplay = ExpReplay(
+        adapter.infer,
+        get_player=get_player,
+        num_parallel_players=NUM_PARALLEL_PLAYERS,
+        state_shape=IMAGE_SIZE,
+        batch_size=BATCH_SIZE,
+        memory_size=MEMORY_SIZE,
+        init_memory_size=INIT_MEMORY_SIZE,
+        update_frequency=UPDATE_FREQ,
+        history_len=FRAME_HISTORY,
+        state_dtype=np.float32
+    )
+    expreplay._before_train()
+    for step_idx, batch in enumerate(expreplay):
+        adapter.train_step(batch)
+        if expreplay.exploration > MIN_EPSILON:
+            expreplay.exploration -= (START_EPSILON - MIN_EPSILON) / STOP_EPSILON_DECAY_AT
+        if step_idx > 0 and step_idx % 5000 == 0:
+            adapter.update_target()
+            mean, max = expreplay.runner.reset_stats()
+            summary_writer.add_summary(
+                tf.Summary(value=(
+                    tf.Summary.Value(tag='expreplay/mean_score', simple_value=mean),
+                    tf.Summary.Value(tag='expreplay/max_score', simple_value=max))),
+                step_idx)
 
 
 if __name__ == '__main__':
