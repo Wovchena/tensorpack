f35bbd599542b7c17f314d79602ff8bf2e43a491
Vladimir <10669582+Wovchena@users.noreply.github.com>
2020-05-30 16:56:33 +0300

    Eager

--- examples/DeepQNetwork/DQN.py
+++ examples/DeepQNetwork/DQN.py
@@ -12,8 +12,8 @@ import gym.wrappers
 import tensorflow as tf
 
 import tensorpack
-import torch
-import torch.nn.functional as F
+# import torch
+# import torch.nn.functional as F
 
 from expreplay import ExpReplay
 
@@ -47,46 +47,24 @@ class Model:
         self._stacked_state_shape = (-1, ) + self.state_shape + (history, )
         self.history = history
         self.num_actions = num_actions
+        self.model = tf.keras.Sequential((
+            tf.keras.layers.Conv2D(32, kernel_size=8, strides=8, activation='relu', input_shape=(84, 75, 4)),
+            tf.keras.layers.Conv2D(64, kernel_size=4, strides=2, activation='relu'),
+            tf.keras.layers.Conv2D(64, kernel_size=3, strides=1, activation='relu'),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(512, activation='relu'),
+            tf.keras.layers.Dense(self.num_actions)))
+        self.target_model = tf.keras.models.clone_model(self.model)
+        self.target_model.set_weights(self.model.get_weights())
 
-    def inputs(self):
-        # When we use h history frames, the current state and the next state will have (h-1) overlapping frames.
-        # Therefore we use a combined state for efficiency:
-        # The first h are the current state, and the last h are the next state.
-        return [tf.TensorSpec((None,) + self.state_shape + (self.history + 1, ), self.state_dtype, 'comb_state'),
-                tf.TensorSpec((None,), tf.int64, 'action'),
-                tf.TensorSpec((None,), tf.float32, 'reward'),
-                tf.TensorSpec((None,), tf.bool, 'isOver')]
-
-    @tensorpack.tfutils.scope_utils.auto_reuse_variable_scope
-    def get_DQN_prediction(self, image):
-        # model = tf.keras.Sequential((
-        #     tf.keras.layers.Conv2D(32, kernel_size=8, strides=8, activation=tf.keras.layers.LeakyReLU(0.01)),
-        #     tf.keras.layers.Conv2D(64, kernel_size=4, strides=2, activation=tf.keras.layers.LeakyReLU(0.01)),
-        #     tf.keras.layers.Conv2D(64, kernel_size=3, strides=1, activation=tf.keras.layers.LeakyReLU(0.01)),
-        #     tf.keras.layers.Dense(512, activation=tf.keras.layers.LeakyReLU(0.01)),
-        #     tf.keras.layers.Dense(self.num_actions)))
-        Q = (tensorpack.LinearWrap(image)
-             .Conv2D('conv0', 32, 8, strides=4)
-             .tf.nn.leaky_relu(0.01)
-             .Conv2D('conv1', 64, 4, strides=2)
-             .tf.nn.leaky_relu(0.01)
-             .Conv2D('conv2', 64, 3)
-             .tf.nn.leaky_relu(0.01)
-             .FullyConnected('fc0', 512)
-             .tf.nn.leaky_relu(0.01)
-             .FullyConnected('fct', self.num_actions)())
-        return tf.identity(Q, name='Qvalue')
 
     def build_graph(self, comb_state, action, reward, isOver):
         comb_state = tf.cast(comb_state, tf.float32)
         input_rank = comb_state.shape.rank
 
-        state = tf.slice(
-            comb_state,
-            [0] * input_rank,
-            [-1] * (input_rank - 1) + [self.history], name='state')
+        state = comb_state[:, :, :, :-1]
 
-        predict_value = self.get_DQN_prediction(state)
+        predict_value = self.model(state)
 
         reward = tf.clip_by_value(reward, -1, 1)
         next_state = tf.slice(
@@ -94,71 +72,51 @@ class Model:
             [0] * (input_rank - 1) + [1],
             [-1] * (input_rank - 1) + [self.history], name='next_state')
         next_state = tf.reshape(next_state, self._stacked_state_shape)
-        action_onehot = tf.one_hot(action, self.num_actions, 1.0, 0.0)
+        action_onehot = tf.one_hot(tf.cast(action, tf.int32), self.num_actions, 1.0, 0.0)
 
         pred_action_value = tf.reduce_sum(predict_value * action_onehot, 1)  # N,
 
-        with tf.variable_scope('target'):
-            targetQ_predict_value = self.get_DQN_prediction(next_state)    # NxA
+        targetQ_predict_value = self.target_model(next_state)    # NxA
 
         best_v = tf.reduce_max(targetQ_predict_value, 1)    # N,
         target = reward + (1.0 - tf.cast(isOver, tf.float32)) * 0.99 * tf.stop_gradient(best_v)
 
-        cost = tf.losses.huber_loss(
-            target, pred_action_value, reduction=tf.losses.Reduction.MEAN)
+        cost = tf.compat.v1.losses.huber_loss(
+            target, pred_action_value, reduction=tf.compat.v1.losses.Reduction.MEAN)
         return cost
-
-
-def update_target_param():
-    vars = tf.global_variables()
-    ops = []
-    G = tf.get_default_graph()
-    for v in vars:
-        target_name = v.op.name
-        if target_name.startswith('target'):
-            new_name = target_name.replace('target/', '')
-            ops.append(v.assign(G.get_tensor_by_name(new_name + ':0')))
-    return tf.group(*ops, name='update_target_network')
 
 
 class TfAdapter:
     def __init__(self, image_size, frame_history):
         self.model = Model(image_size, frame_history, get_player().action_space.n)
-        self.placeholders = tuple(tf.placeholder(tensor_spec.dtype, shape=tensor_spec.shape, name=tensor_spec.name)
-                             for tensor_spec in self.model.inputs())
-        self.opt = tf.train.RMSPropOptimizer(1e-3, decay=0.95, momentum=0.95, epsilon=1e-2)
-        self.train_op = self.opt.minimize(self.model.build_graph(*self.placeholders))
+        self.opt = tf.keras.optimizers.RMSprop(1e-3, decay=0.95, momentum=0.95, epsilon=1e-2)
 
-        self.update_target_op = update_target_param()
-
-        with tf.name_scope('tower-pred-0/'):
-            self.model.build_graph(*self.placeholders)
-        self.sess = tf.Session()
-        self.sess.run(tf.initialize_all_variables())
-
-        self.infer = self.sess.make_callable(fetches=['tower-pred-0/Qvalue:0'], feed_list=['tower-pred-0/state:0'])
+    def infer(self, state):
+        return self.model.model(tf.convert_to_tensor(state))
 
     def train_step(self, batch):
-        self.sess.run(self.train_op, feed_dict=dict(zip(self.placeholders, batch)))
+        with tf.GradientTape() as tape:
+            loss = self.model.build_graph(*batch)
+        gradients = tape.gradient(loss, self.model.model.trainable_variables)
+        self.opt.apply_gradients(zip(gradients, self.model.model.trainable_variables))
 
     def update_target(self):
-        with self.sess.as_default():
-            self.update_target_op.run()
+        self.model.target_model.set_weights(self.model.model.get_weights())
 
 
-class DQN(torch.nn.Module):
-    def __init__(self, state_dim, n_actions):
-        super().__init__()
-        self.conv1 = torch.nn.Conv2d(state_dim, 32, kernel_size=8, stride=4, padding=4)
-        self.conv2 = torch.nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=2)
-        self.conv3 = torch.nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.linear1 = torch.nn.Linear(7680, 512)
-        self.linear2 = torch.nn.Linear(512, n_actions)
-
-    def forward(self, state):
-        c1 = torch.nn.functional.leaky_relu(self.conv3(torch.nn.functional.leaky_relu(self.conv2(torch.nn.functional.leaky_relu(self.conv1(state))))))
-        fc1 = F.leaky_relu(self.linear1(c1.view(c1.shape[0], -1)))
-        return self.linear2(fc1)
+# class DQN(torch.nn.Module):
+    # def __init__(self, state_dim, n_actions):
+        # super().__init__()
+        # self.conv1 = torch.nn.Conv2d(state_dim, 32, kernel_size=8, stride=4, padding=4)
+        # self.conv2 = torch.nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=2)
+        # self.conv3 = torch.nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        # self.linear1 = torch.nn.Linear(7680, 512)
+        # self.linear2 = torch.nn.Linear(512, n_actions)
+# 
+    # def forward(self, state):
+        # c1 = torch.nn.functional.leaky_relu(self.conv3(torch.nn.functional.leaky_relu(self.conv2(torch.nn.functional.leaky_relu(self.conv1(state))))))
+        # fc1 = F.leaky_relu(self.linear1(c1.view(c1.shape[0], -1)))
+        # return self.linear2(fc1)
 
 
 class TorchAdapter:
@@ -209,7 +167,7 @@ def main():
 
     adapter = TfAdapter(IMAGE_SIZE, FRAME_HISTORY)
     # adapter = TorchAdapter()
-    summary_writer = tf.summary.FileWriter(datetime.datetime.now().strftime('logs/%d-%m-%Y_%H-%M'))
+    summary_writer = tf.summary.create_file_writer(datetime.datetime.now().strftime('logs/%d-%m-%Y_%H-%M'))
     expreplay = ExpReplay(
         adapter.infer,
         get_player=get_player,
@@ -230,11 +188,10 @@ def main():
         if step_idx > 0 and step_idx % 5000 == 0:
             adapter.update_target()
             mean, max = expreplay.runner.reset_stats()
-            summary_writer.add_summary(
-                tf.Summary(value=(
-                    tf.Summary.Value(tag='expreplay/mean_score', simple_value=mean),
-                    tf.Summary.Value(tag='expreplay/max_score', simple_value=max))),
-                step_idx)
+            with summary_writer.as_default():
+                tf.summary.scalar('expreplay/mean_score', mean, step_idx)
+                tf.summary.scalar('expreplay/max_score', max, step_idx)
+                summary_writer.flush()
 
 
 if __name__ == '__main__':
